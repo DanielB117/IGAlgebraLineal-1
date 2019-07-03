c4380b6435cb7ed0d66ac08247af2bc2814387dd
DanielB117 <daniel.betancourth@ujcv.edu.hn>
2019-07-03 08:00:19 -0600

    Agregando miembros privados al vector en R2

--- src/edu/ujcv/progra2/matriz/IGMatR2.java
+++ src/edu/ujcv/progra2/matriz/IGMatR2.java
@@ -2,4 +2,30 @@ package edu.ujcv.progra2.matriz;
 
 public class IGMatR2 {
     // TODO: implementar.
+
+    //en r2
+    //double x1,y1;
+    //double x2,y2;
+
+//en r3
+    //double x1,y1,z1;
+    //double x2,y2,z2;
+    //double x3,y3,z3;
+
+
+    //en r4
+    //double x1,y1,z1,w1;
+    //double x2,y2,z2,w2;
+    //double x3,y3,z3,w3;
+    //double x4,y4,z4,w4;
+
+    //escoge una de estras tres formas
+    //double[][] m;
+
+    //IGVecR2 fila1;
+    //IGVecR2 fila2;
+
+    //IGVecR2 columna1;
+    //IGVecR2 columna2;
+
 }
