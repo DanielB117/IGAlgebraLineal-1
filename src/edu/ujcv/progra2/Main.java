891245eeeeafbbf1241df5a6d29a7c75aa3e4d78
DanielB117 <daniel.betancourth@ujcv.edu.hn>
2019-07-04 19:45:40 -0600

    Agregando el menu donde el usuario escoga lo que desee calcular

--- src/edu/ujcv/progra2/Main.java
+++ src/edu/ujcv/progra2/Main.java
@@ -1,8 +1,51 @@
 package edu.ujcv.progra2;
 
-public class Main {
+import javax.crypto.spec.OAEPParameterSpec;
 
+public class Main {
     public static void main(String[] args) {
 	// write your code here
+
+
+
+System.out.println("que desea calcular");
+ int OAC;
+        System.out.println("presione 1 para calcular vectores en R2");
+        System.out.println("presione 2 para calcular vectores en R3");
+        System.out.println("presione 3 para calcular vectores en R4");
+        System.out.println("presione 4 para calcular matrices en R2");
+        System.out.println("presione 5 para calcular matrices en R3");
+        System.out.println("presione 6 para calcular matrices en R4");
+
+        if (OAC == 1) {
+
+
+        }
+
+        if (OAC == 2) {
+
+
+        }
+        if (OAC == 3) {
+
+
+        }
+        if (OAC== 4) {
+
+
+        }
+        if (OAC== 5) {
+
+
+        }
+        if (OAC == 6) {
+
+
+        }
+
+
+
+
+
     }
 }
